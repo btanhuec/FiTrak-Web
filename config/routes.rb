d0426555d992b9e0acdf2c4b1fef6885eee4509e
Joshua  Tanhueco <btanhuec@ucsc.edu>
2019-10-20 04:40:18 -0700

    login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :pages
+
+  devise_scope :user do
+    authenticated :user do
+      root 'pages#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
 end
