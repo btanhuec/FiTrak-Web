36e03af2421e37ccbdb8c1edcec6390a35c6a287
Brix Tanhueco <btanhuec@ucsc.edu>
2019-11-24 14:56:53 -0800

    final dashboard UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_014537) do
+ActiveRecord::Schema.define(version: 2019_11_20_012515) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "excercises", force: :cascade do |t|
     t.string "title"
